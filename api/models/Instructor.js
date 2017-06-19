60f5daf09abde48e492ec1b39275a6779cb43627
Katy DiPasquale <katy.dipasquale@libertymutual.com>
2017-06-19 10:40:29 -0400

    initial commit

---
+++ api/models/Instructor.js
@@ -0,0 +1,43 @@
+/**
+ * Instructor.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    instructor_id: {
+      type: "integer",
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: "string",
+      minLength: 2,
+      maxLength: 30
+    },
+    last_name: {
+      type: "string",
+      minLength: 2,
+      maxLength: 30
+    },
+    major_id: {
+      type: 'integer',
+      maxLength: 10
+    },
+    years_of_experience: {
+      type: 'integer',
+      maxLength: 2
+    },
+    tenured: {
+      type: 'integer',
+      maxLength: 1
+    }
+
+
+  },
+  autoPK: false
+
+};
---
+++ config/models.js
@@ -0,0 +1,36 @@
+/**
+ * Default model configuration
+ * (sails.config.models)
+ *
+ * Unless you override them, the following properties will be included
+ * in each of your models.
+ *
+ * For more info on Sails models, see:
+ * http://sailsjs.org/#!/documentation/concepts/ORM
+ */
+
+module.exports.models = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Your app's default connection. i.e. the name of one of your app's        *
+  * connections (see `config/connections.js`)                                *
+  *                                                                          *
+  ***************************************************************************/
+  connection: 'someMysqlServer',
+  //once we set the connection in the local.js, then we come here and change it to match
+
+  /***************************************************************************
+  *                                                                          *
+  * How and whether Sails will attempt to automatically rebuild the          *
+  * tables/collections/etc. in your schema.                                  *
+  *                                                                          *
+  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
+  *                                                                          *
+  ***************************************************************************/
+  migrate: 'safe', //don't allow changes to my schema
+  schema: true,  //schema means ignore the junk fields, enforce the existing fields
+  autoCreatedAt: false, //don't create these time stamps
+  autoUpdatedAt: false
+
+};
